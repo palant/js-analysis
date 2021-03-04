7dea6b118c6f99fac26fc5f1918d55804f342587
Wladimir Palant <fqcgithub@palant.de>
2021-03-04 11:09:55 +0100

    Added handling of one more Webpack variant

--- test/bundles.js
+++ test/bundles.js
@@ -385,4 +385,46 @@ describe("bundles.parseModules()", () =>
       module.exports = 42;
     `).body);
   });
+
+  it("should recognize JSONP chunks with additional parameter", () =>
+  {
+    let ast = parseScript(`
+      (window.webpackJsonp = window.webpackJsonp || []).push([
+        ["abc", "cda"],
+        {
+          "abc": function(m, e, r)
+          {
+            e.test = function()
+            {
+              return r("123");
+            };
+          },
+          "123": function(m, e, r)
+          {
+            m.exports = 42;
+          }
+        },
+        [["xyz", "zyx"]]
+      ]);`);
+
+    let modules = new Map();
+    for (let {name, node, scope} of parseModules(ast))
+      modules.set(name, node);
+
+    expect(modules.size).to.equal(2);
+
+    console.log(modules);
+    expect(modules.has("/abc")).to.be.true;
+    expect(modules.get("/abc").body).to.deep.equal(parseScript(`
+      exports.test = function()
+      {
+        return require("123");
+      };
+    `).body);
+
+    expect(modules.has("/123")).to.be.true;
+    expect(modules.get("/123").body).to.deep.equal(parseScript(`
+      module.exports = 42;
+    `).body);
+  });
 });
